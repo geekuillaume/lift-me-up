162b8b1ff82a6ba76eb08a25973d13454f448488
Neamar <neamar@neamar.fr>
2016-12-25 19:38:02 +0100

    addLift / addFloor

--- __tests__/models/building.js
+++ __tests__/models/building.js
@@ -3,11 +3,33 @@
 var assert = require("assert");
 
 var Building = require('../../scripts/models/building');
+var Lift = require('../../scripts/models/lift');
+var Floor = require('../../scripts/models/floor');
 
 describe("Building model", () => {
-  it("should let user create new building with empty holders", () => {
+  it("should create new building with empty holders", () => {
     var building = new Building();
     assert.deepEqual(building.floors, []);
     assert.deepEqual(building.lifts, []);
+  });
+
+  it("should have a working addLift() command", () => {
+    var building = new Building();
+    assert.deepEqual(building.lifts, []);
+
+    var lift = new Lift(building);
+    building.addLift(lift);
+
+    assert.deepEqual(building.lifts, [lift]);
+  });
+
+  it("should have a working addFloor() command", () => {
+    var building = new Building();
+    assert.deepEqual(building.floors, []);
+
+    var floor = new Floor(building);
+    building.addFloor(floor);
+
+    assert.deepEqual(building.floors, [floor]);
   });
 });
--- scripts/models/building.js
+++ scripts/models/building.js
@@ -4,3 +4,11 @@ module.exports = function Building() {
   this.floors = [];
   this.lifts = [];
 };
+
+module.exports.prototype.addLift = function addLift(lift) {
+  this.lifts.push(lift);
+};
+
+module.exports.prototype.addFloor = function addLift(floor) {
+  this.floors.push(floor);
+};
